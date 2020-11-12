4dce76f89f0ec290e82e484b495ed3fb22accd62
jairoblatt <jairoblattc@gmail.com>
2020-11-12 19:36:16 -0300

    Navigation drawer mobile

--- src/components/NavigationDrawer/NavigationDrawer.vue
+++ src/components/NavigationDrawer/NavigationDrawer.vue
@@ -1,25 +1,35 @@
 <template>
-  <aside class="navigation-drawer-container">
-    <div class="navigation-drawer-logo">S.</div>
-    <div class="navigation-drawer-items">
-      <ul>
-        <li v-for="(item, index) in items" :key="index">
-          <router-link
-            tag="a"
-            exact-active-class="active"
-            :to="{ name: item.route }"
-          >
-            <i :class="item.icon"></i>
-          </router-link>
-        </li>
-      </ul>
+  <span>
+    <!-- App bar -->
+    <div class="app-bar" v-if="activeAppBar">
+      <button @click="toggleDrawer">
+        <i class="mdi mdi-menu"></i>
+      </button>
     </div>
-    <div class="navigation-drawer-logout">
-      <router-link tag="span" :to="{ name: 'Login' }"
-        ><i class="mdi mdi-logout"></i
-      ></router-link>
-    </div>
-  </aside>
+
+    <!-- Navigation drawer -->
+    <s-transition transition="slide-left">
+      <aside v-show="drawer || !activeAppBar" :class="navigationContainerClasses">
+        <div class="navigation-drawer-logo">S.</div>
+        <div class="navigation-drawer-items">
+          <ul>
+            <li v-for="(item, index) in items" :key="index">
+              <router-link
+                tag="a"
+                exact-active-class="active"
+                :to="{ name: item.route }">
+                <i :class="item.icon"></i>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+        <div class="navigation-drawer-logout">
+          <router-link tag="span" :to="{ name: 'Login' }"><i class="mdi mdi-logout"></i></router-link>
+        </div>
+      </aside>
+    </s-transition>
+
+  </span>
 </template>
 <script>
 const items = [
@@ -48,10 +58,52 @@ const items = [
     route: "Dashboard",
   },
 ];
+
+
 export default {
   data: () => ({
     items,
+    drawer: false,
+    window:{
+      width: 0
+    }
   }),
+
+  computed: {
+    navigationContainerClasses(){
+      return {
+        'navigation-drawer-container':true,
+        'isOpen-drawer':this.activeAppBar,
+      }
+    },
+    activeAppBar(){
+      if(this.window.width > 952) return false;
+      return true;
+    }
+  },
+
+  created() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  },
+
+  destroyed() {
+      window.removeEventListener('resize', this.handleResize);
+  },
+
+  watch: {
+    $route: "toggleDrawer",
+  },
+
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    },
+
+    handleResize() {
+      this.window.width = window.innerWidth;
+    },
+  },
 };
 </script>
 <style lang="scss" src="./NavigationDrawer.scss"></style>
