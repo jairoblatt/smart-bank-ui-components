dd2b6cefc6c84ce087a76b5eef44945a866afcd3
jairoblatt <jairoblattc@gmail.com>
2021-03-22 19:56:05 -0300

    add: app loader screen

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,29 +1,21 @@
 <template>
   <div class="layout-container container-fluid">
-
-    <!-- Navigation drawer -->
     <div class="layout-navigation-drawer">
       <navigation-drawer />
     </div>
-
-    <!-- Content -->
     <div class="layout-content-page">
       <div class="row">
         <child />
       </div>
     </div>
-    
   </div>
 </template>
 
 <script>
-
-import NavigationDrawer from "@/components/NavigationDrawer";
+import NavigationDrawer from '@/components/NavigationDrawer';
 export default {
-
   components: {
     NavigationDrawer,
   },
-
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,68 +1,70 @@
-import Vue from 'vue'
-import routes from './routes'
-import Router from 'vue-router'
+import Vue from 'vue';
+import routes from './routes';
+import Router from 'vue-router';
 
-Vue.use(Router)
-const router = createRouter()
+Vue.use(Router);
+const router = createRouter();
 
-// Create router
 function createRouter() {
   const router = new Router({
     scrollBehavior,
     mode: 'history',
-    routes
-  })
-  router.beforeEach(beforeEach)
-  router.afterEach(afterEach)
-  return router
+    routes,
+  });
+  router.beforeEach(beforeEach);
+  router.afterEach(afterEach);
+  return router;
 }
 
-//Scroll Behavior
 function scrollBehavior(to, from, savedPosition) {
   if (savedPosition) return savedPosition;
   return { x: 0, y: 0 };
 }
 
-//Global router guard.
 async function beforeEach(to, from, next) {
-  let components = []
+  let components = [];
   try {
-    // Get the matched components and resolve them.
-    components = await resolveComponents(router.getMatchedComponents({ ...to }))
+    components = await resolveComponents(
+      router.getMatchedComponents({ ...to })
+    );
   } catch (error) {
     if (/^Loading( CSS)? chunk (\d)+ failed\./.test(error.message)) {
-      window.location.reload(true)
-      return
+      window.location.reload(true);
+      return;
     }
   }
-  if (components.length === 0) return next()
+  if (components.length === 0) return next();
   const { setLayout, $loading } = appChildren();
-  setLayout(components[0] || '')
-  // Start the loading bar.
+  setLayout(components[0] || '');
   if (components[components.length - 1].loading !== false) {
-    router.app.$nextTick(() => $loading.start())
+    router.app.$nextTick(() => $loading.start());
   }
-  next()
+  next();
 }
 
-
-// Global after hook.
 async function afterEach(to, from, next) {
   await router.app.$nextTick();
   const { $loading } = appChildren();
   $loading.finish();
+
+  setTimeout(disableAppLoader, 500);
 }
 
-//Resolve async components.
 function resolveComponents(components) {
-  return Promise.all(components.map(component => {
-    return typeof component === 'function' ? component() : component
-  }))
+  return Promise.all(
+    components.map(component => {
+      return typeof component === 'function' ? component() : component;
+    })
+  );
 }
 
-// Return children app.vue
-function appChildren(){
+function appChildren() {
   return router.app.$children[0];
+}
+
+function disableAppLoader() {
+  const appLoader = document.querySelector('[data-app="loader"]');
+  if (appLoader) appLoader.style.display = 'none';
 }
 
 export default router;
